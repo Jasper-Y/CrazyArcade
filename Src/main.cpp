29d497a295863699a1e104f06aae6dd2d480fae9
cj.yu <chengjiyu.sjtu@gmail.com>
2021-11-13 23:54:25 -0500

    feat: add simulation for buffer

--- Src/main.cpp
+++ Src/main.cpp
@@ -89,6 +89,9 @@ void ExecuteCommand(GameManager *manager) {
                 printf("[Game overrrr]\n");
                 break;
             }
+
+            // Represent the operation cost for rendering
+            std::this_thread::sleep_for(std::chrono::milliseconds(1000));
         }
         std::this_thread::sleep_for(std::chrono::milliseconds(10));
     }
